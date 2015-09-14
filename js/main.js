e6c501ebab34a34039543d558411c61410cfcacc
marb <marb61a@gmail.com>
2015-09-14 13:26:27 +0000

    updated main.js to show and add

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ $(document).ready(function(){
     	request.onsuccess = function(e){
     		console.log('Success: Opened Database...');
     		db = e.target.result;
+    		showCustomers();
     	};
     	
     	//Error
@@ -24,3 +25,64 @@ $(document).ready(function(){
     		console.log('Error: Could Not Open Database...');
     	};
 });
+
+
+// Add customer
+function addCustomer(){
+    var name = $('#name').val();
+	var email = $('#email').val();
+	
+	var transaction = db.transaction(["customers"],"readwrite");
+	
+	// Ask for objectstore
+	var store = transaction.objectStore("customers");
+	
+	// Define customer
+	var customer = {
+		name: name,
+		email: email
+	};
+	
+	// Perform the customer addition
+	var request = store.add(customer);
+	
+	//Success
+	request.onsuccess = function(e){
+		window.location.href="index.html";
+	};
+	
+	//Error
+	request.onerror = function(e){
+		alert("Sorry, the customer was not added");
+		console.log('Error', e.target.error.name);
+	};
+	
+}
+
+
+// Display customers
+function showCustomers(e){
+    var transaction = db.transaction(["customers"],"readonly");
+    
+    // Ask for objectstore
+    var store = transaction.objectStore("customers");
+	var index = store.index('name');
+	
+	var output = '';
+	
+	index.openCursor = function(e){
+	    var cursor = e.target.result;
+	    
+	    if(cursor){
+			output += "<tr id='customer_"+cursor.value.id+"'>";
+			output += "<td>"+cursor.value.id+"</td>";
+			output += "<td><span class='cursor customer'>"+cursor.value.name+"</span></td>";
+			output += "<td><span class='cursor customer'>"+cursor.value.email+"</span></td>";
+			output += "<td><a href=''>Delete</a></td>";
+			output += "</tr>";
+			cursor.continue();
+		}
+		
+		$('#customers').html(output);
+	};
+}
